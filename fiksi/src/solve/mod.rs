6178a0b32cb3da7afa46e49e4faa9b28d9967018
Thomas Churchman <thomas@churchman.nl>
2025-06-23 16:08:06 +0200

    L-BFGS with Hager-Zhang line solver

--- fiksi/src/solve/mod.rs
+++ fiksi/src/solve/mod.rs
@@ -1,6 +1,23 @@
 // Copyright 2025 the Fiksi Authors
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
+//! Optimization and solving routines.
+
+mod lbfgs;
 mod lm;
 
+pub(crate) use lbfgs::*;
 pub(crate) use lm::*;
+
+/// Numerical optimization algoritihms.
+pub enum Optimizer {
+    /// The [Levenberg-Marquardt][lm] algorithm.
+    ///
+    /// [lm]: https://en.wikipedia.org/wiki/Levenberg%E2%80%93Marquardt_algorithm
+    LevenbergMarquardt,
+
+    /// The [limited-memory BFGS][wikipedia] algorithm.
+    ///
+    /// [wikipedia]: https://en.wikipedia.org/wiki/Limited-memory_BFGS
+    LBfgs,
+}
