c750fafd41ec52d9ed9355ca4212c8e6a13dd237
Tom Churchman <thomas@churchman.nl>
2025-09-01 10:10:11 +0200

    Better reuse allocations in LM, use specialized multiplication (#91)
    Relative to the state in #90, this is a ~25 to ~30% improvement in
    throughput.
    
    Because of using `ColPivQR::q_tr_mul` (as a more performant specialized
    multiplication rather than manually calculating `Q^T r` using the normal
    operations) the math in badly conditioned cases can fall slightly
    differently, hence one of the tests for big differences in magnitude
    needs to be loosened a bit. I'm not sure this specialized method is
    necessarily worse in such cases, as the tests are right up against the
    limit, meaning we may just have been lucky with the previous computation
    succeeding.
    
    ```
    solve/hinged_triangles/hinged_triangles_size=1
                            time:   [4.1490 µs 4.1567 µs 4.1700 µs]
                            thrpt:  [239.81 Kelem/s 240.57 Kelem/s 241.02 Kelem/s]
                     change:
                            time:   [−22.336% −22.066% −21.764%] (p = 0.00 < 0.05)
                            thrpt:  [+27.819% +28.314% +28.760%]
                            Performance has improved.
    ```
    
    ```
    solve/hinged_triangles/hinged_triangles_size=4
                            time:   [24.823 µs 24.869 µs 24.919 µs]
                            thrpt:  [160.52 Kelem/s 160.84 Kelem/s 161.14 Kelem/s]
                     change:
                            time:   [−26.597% −26.437% −26.298%] (p = 0.00 < 0.05)
                            thrpt:  [+35.682% +35.938% +36.234%]
                            Performance has improved.
    ```
    
    ```
    solve/hinged_triangles/hinged_triangles_size=16
                            time:   [1.1153 ms 1.1166 ms 1.1176 ms]
                            thrpt:  [14.316 Kelem/s 14.329 Kelem/s 14.346 Kelem/s]
                     change:
                            time:   [−23.536% −23.434% −23.333%] (p = 0.00 < 0.05)
                            thrpt:  [+30.434% +30.606% +30.780%]
                            Performance has improved.
    ```
    
    ```
    solve/hinged_triangles/hinged_triangles_size=64
                            time:   [77.178 ms 77.377 ms 77.676 ms]
                            thrpt:  [823.93  elem/s 827.12  elem/s 829.26  elem/s]
                     change:
                            time:   [−21.493% −21.197% −20.862%] (p = 0.00 < 0.05)
                            thrpt:  [+26.362% +26.899% +27.377%]
                            Performance has improved.
    ```

--- fiksi/src/solve/lm.rs
+++ fiksi/src/solve/lm.rs
@@ -19,9 +19,6 @@ use crate::{
 /// subsystem. The variables given by the elements in `element_set` are seen as free, other
 /// variables are seen as fixed parameters.
 pub(crate) fn levenberg_marquardt(variables: &mut [f64], subsystem: &Subsystem<'_>) {
-    // TODO: this is allocation-happy.
-    // TODO: we don't use enough of nalgebra here to justify including that dependency.
-
     let mut variables_scratch = variables.to_vec();
     let variables_scratch = variables_scratch.as_mut_slice();
 
@@ -29,11 +26,22 @@ pub(crate) fn levenberg_marquardt(variables: &mut [f64], subsystem: &Subsystem<'
     let mut residuals = nalgebra::DVector::zeros(subsystem.expressions().len());
     let mut residuals_scratch = nalgebra::DVector::zeros(subsystem.expressions().len());
 
-    // All first-order partial derivatives of the expressions. This is a dense representation (each
-    // pair of expression and variable has a partial derivative represented here, even for
-    // variables that do not contribute to the expression). It's possible a sparse representation
-    // may be more efficient in certain cases.
+    // All first-order partial derivatives of the expressions, in row-major order. This is a dense
+    // representation (each pair of expression and variable has a partial derivative represented
+    // here, even for variables that do not contribute to the expression). It's possible a sparse
+    // representation may be more efficient in certain cases.
     let mut jacobian = vec![0.; subsystem.expressions().len() * subsystem.free_variables().len()];
+
+    // The same Jacobian, but a separate allocation for the column-major representation, which
+    // nalgebra expects. TODO: perhaps make everything use a column-major allocation, as that's
+    // somewhat more common (making the per-expression gradients have a stride instead).
+    let mut jacobian_ = nalgebra::DMatrix::zeros(
+        subsystem.expressions().len(),
+        subsystem.free_variables().len(),
+    );
+
+    // The (augmented) residual matrix.
+    let mut r_aug = nalgebra::DMatrix::zeros(jacobian_.nrows() + jacobian_.ncols(), 1);
 
     calculate_residuals_and_jacobian(
         subsystem,
@@ -50,12 +58,7 @@ pub(crate) fn levenberg_marquardt(variables: &mut [f64], subsystem: &Subsystem<'
             break;
         }
 
-        // Clone the Jacobian to a nalgebra matrix for now.
-        // TODO: remove
-        let mut jacobian_ = nalgebra::DMatrix::zeros(
-            subsystem.expressions().len(),
-            subsystem.free_variables().len(),
-        );
+        // Copy from the row-major Jacobian to the column-major Jacobian.
         for i in 0..subsystem.expressions().len() {
             for j in 0..subsystem.free_variables().len() {
                 jacobian_[(i, j)] = jacobian[i * subsystem.free_variables().len() + j];
@@ -85,8 +88,13 @@ pub(crate) fn levenberg_marquardt(variables: &mut [f64], subsystem: &Subsystem<'
             // ill-conditioned cases, the lack of stability due to forming the square can lead to
             // having to perform many more iterations to converge, as well as failure to convergence at
             // all.
+            //
+            // (It seems that with the current nalgebra API, it's not entirely straightforward to
+            // reuse the allocation for `j_aug`. It's moved into `ColPivQR`, and moving it out
+            // again requires an expensive resizing operation that copies elements around.)
             let mut j_aug =
                 nalgebra::DMatrix::zeros(jacobian_.nrows() + jacobian_.ncols(), jacobian_.ncols());
+
             j_aug.rows_mut(0, jacobian_.nrows()).copy_from(&jacobian_);
             // Augment by the damping factor `lambda`.
             for idx in 0..jacobian_.ncols() {
@@ -95,14 +103,22 @@ pub(crate) fn levenberg_marquardt(variables: &mut [f64], subsystem: &Subsystem<'
             // The augmented residual matrix:
             // [ r ]
             // [ 0 ]
-            let mut r_aug = nalgebra::DMatrix::zeros(jacobian_.nrows() + jacobian_.ncols(), 1);
-            r_aug.rows_mut(0, jacobian_.nrows()).copy_from(&residuals);
+            {
+                let mut r = r_aug.rows_mut(0, jacobian_.nrows());
+                r.copy_from(&residuals);
+                r.neg_mut();
+            }
+            r_aug
+                .rows_mut(jacobian_.nrows(), jacobian_.ncols())
+                .fill(0.);
 
             let qr = j_aug.col_piv_qr();
-            let Some(mut delta) = qr
-                .r()
-                .solve_upper_triangular(&(-qr.q().transpose() * r_aug))
-            else {
+            qr.q_tr_mul(&mut r_aug);
+
+            let mut delta = r_aug.rows_mut(0, jacobian_.ncols());
+            // Note: we'd like to use qr.unpack_r() here, but there may be some UB in `nalgebra`:
+            // <https://github.com/endoli/fiksi/pull/91>.
+            if !qr.r().solve_upper_triangular_mut(&mut delta) {
                 lambda *= 8.;
                 continue;
             };
