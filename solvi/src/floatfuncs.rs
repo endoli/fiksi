ba3f5e573566202b0c8c2c4c8c74739999117d6a
Tom Churchman <thomas@churchman.nl>
2025-10-23 13:43:21 +0200

    Initialize sparse linear algebra crate (#114)

---
+++ solvi/src/floatfuncs.rs
@@ -0,0 +1,56 @@
+// Copyright 2024 the Fiksi Authors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+//! Shims for math functions that ordinarily come from std.
+
+/// Defines a trait that chooses between libstd or libm implementations of float methods.
+macro_rules! define_float_funcs {
+    ($(
+        fn $name:ident(self $(,$arg:ident: $arg_ty:ty)*) -> $ret:ty
+            => $lname:ident/$lfname:ident;
+    )+) => {
+
+        /// Since core doesn't depend upon libm, this provides libm implementations
+        /// of float functions which are typically provided by the std library, when
+        /// the `std` feature is not enabled.
+        ///
+        /// For documentation see the respective functions in the std library.
+        #[cfg(not(feature = "std"))]
+        #[expect(dead_code, reason = "Tasteful YAGNI.")]
+        pub(crate) trait FloatFuncs : Sized {
+            $(fn $name(self $(,$arg: $arg_ty)*) -> $ret;)+
+        }
+
+        #[cfg(not(feature = "std"))]
+        impl FloatFuncs for f32 {
+            $(fn $name(self $(,$arg: $arg_ty)*) -> $ret {
+                #[cfg(feature = "libm")]
+                return libm::$lfname(self $(,$arg)*);
+
+                #[cfg(not(feature = "libm"))]
+                compile_error!("Fiksi requires either the `std` or `libm` feature")
+            })+
+        }
+
+        #[cfg(not(feature = "std"))]
+        impl FloatFuncs for f64 {
+            $(fn $name(self $(,$arg: $arg_ty)*) -> $ret {
+                #[cfg(feature = "libm")]
+                return libm::$lname(self $(,$arg)*);
+
+                #[cfg(not(feature = "libm"))]
+                compile_error!("Fiksi requires either the `std` or `libm` feature")
+            })+
+        }
+    }
+}
+
+define_float_funcs! {
+    fn cbrt(self) -> Self => cbrt/cbrtf;
+    fn ceil(self) -> Self => ceil/ceilf;
+    fn floor(self) -> Self => floor/floorf;
+    // Note: powi is missing because its libm implementation is not efficient
+    fn powf(self, n: Self) -> Self => pow/powf;
+    fn round(self) -> Self => round/roundf;
+    fn sqrt(self) -> Self => sqrt/sqrtf;
+}
