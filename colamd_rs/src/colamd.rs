f744bdddb3bd4ded0cfc4079e27c03d1c9031fcb
Tom Churchman <thomas@churchman.nl>
2025-11-22 14:00:59 +0100

    colamd: fix `clippy::nonminimal_bool` (#160)

--- colamd_rs/src/colamd.rs
+++ colamd_rs/src/colamd.rs
@@ -9,7 +9,6 @@
     non_upper_case_globals,
     unused_assignments,
     unsafe_op_in_unsafe_fn,
-    clippy::nonminimal_bool,
     clippy::toplevel_ref_arg,
     reason = "transpiled using c2rust"
 )]
@@ -777,7 +776,7 @@ unsafe extern "C" fn init_scoring(
     }
     c = n_col - 1 as int32_t;
     while c >= 0 as int32_t {
-        if !((*Col.offset(c as isize)).start < ALIVE as int32_t) {
+        if (*Col.offset(c as isize)).start >= ALIVE as int32_t {
             deg = (*Col.offset(c as isize)).length;
             if deg > dense_col_count {
                 n_col2 -= 1;
@@ -808,7 +807,7 @@ unsafe extern "C" fn init_scoring(
     }
     c = n_col - 1 as int32_t;
     while c >= 0 as int32_t {
-        if !((*Col.offset(c as isize)).start < ALIVE as int32_t) {
+        if (*Col.offset(c as isize)).start >= ALIVE as int32_t {
             score = 0 as core::ffi::c_int as int32_t;
             cp = &mut *A.offset((*Col.offset(c as isize)).start as isize) as *mut int32_t;
             new_cp = cp;
@@ -1139,7 +1138,7 @@ unsafe extern "C" fn order_children(n_col: int32_t, Col: *mut Colamd_Col, p: *mu
     let mut order: int32_t = 0;
     i = 0 as core::ffi::c_int as int32_t;
     while i < n_col {
-        if !((*Col.offset(i as isize)).start == DEAD_PRINCIPAL as int32_t)
+        if ((*Col.offset(i as isize)).start != DEAD_PRINCIPAL as int32_t)
             && (*Col.offset(i as isize)).shared2.order == EMPTY as int32_t
         {
             parent = i;
@@ -1157,7 +1156,7 @@ unsafe extern "C" fn order_children(n_col: int32_t, Col: *mut Colamd_Col, p: *mu
                 (*Col.offset(c as isize)).shared2.order = fresh41;
                 (*Col.offset(c as isize)).shared1.parent = parent;
                 c = (*Col.offset(c as isize)).shared1.parent;
-                if !((*Col.offset(c as isize)).shared2.order == EMPTY as int32_t) {
+                if (*Col.offset(c as isize)).shared2.order != EMPTY as int32_t {
                     break;
                 }
             }
